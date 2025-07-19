720108fe7c783e5035eab45395673656eea9a4a8
Mahjabeen-sarfraz <sarfrazahmadbaig15900@gmail.com>
2025-07-19 03:53:44 -0700

    ch

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 console.log("Hello Github")
+// global scope
+// fumctional scope
+// block scope
+
+// let, const // fumctional or block scope
+// var functional
+
+sum()
+
+function sum() {
+
+    if (true) {
+        let num = 30
+        var num2 = 70
+        const num3 = 80
+        console.log(num, "num from")
+        console.log(num3, "num3 from")
+    }
+    console.log(num2, "num2 from")
+}
+
+
+
+
+// if (num > 0) {
+//     let handle = 9
+//     console.log(handle)
+// }
+
+
+
+
+console.log(abc)
+var abc = "abc"
